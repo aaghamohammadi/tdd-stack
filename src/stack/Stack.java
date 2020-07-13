6d6e250aee2e50cafcf38216eb76af7445f4007e
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2020-07-13 07:08:23 +0430

    Empty stack should have zero size

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,4 +1,7 @@
 package stack;
 
 public class Stack {
+    public boolean isEmpty() {
+        return true;
+    }
 }
--- src/test/StackTest.java
+++ src/test/StackTest.java
@@ -1,14 +1,14 @@
 package test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import stack.Stack;
 
-import static org.junit.Assert.fail;
 
 public class StackTest {
     @Test
     public void emptyStackShouldHaveZeroSize() {
         Stack stack = new Stack();
-        fail();
+        Assert.assertTrue(stack.isEmpty());
     }
 }
