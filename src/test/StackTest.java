6bec3823007254782471307b281f050726033578
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2020-07-13 07:01:54 +0430

    Create the first failed test

--- src/test/StackTest.java
+++ src/test/StackTest.java
@@ -1,4 +1,14 @@
 package test;
 
+import org.junit.Test;
+import stack.Stack;
+
+import static org.junit.Assert.fail;
+
 public class StackTest {
+    @Test
+    public void emptyStackShouldHaveZeroSize() {
+        Stack stack = new Stack();
+        fail();
+    }
 }
